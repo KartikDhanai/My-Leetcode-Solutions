23ba2f171a158711c4fff9b9e0d63de790f74a29
Kartik Dhanai <94917152+KartikDhanai@users.noreply.github.com>
2024-02-06 18:39:30 +0530

    Create 3_SUM.cpp

---
+++ 3_SUM.cpp
@@ -0,0 +1,91 @@
+Triplet Sum in Array
+
+Given an array arr of size n and an integer X. Find if there's a triplet in the array which sums up to the given integer X.
+
+
+Example 1:
+
+Input:
+n = 6, X = 13
+arr[] = [1 4 45 6 10 8]
+Output:
+1
+Explanation:
+The triplet {1, 4, 8} in 
+the array sums up to 13.
+Example 2:
+
+Input:
+n = 5, X = 10
+arr[] = [1 2 4 3 6]
+Output:
+1
+Explanation:
+The triplet {1, 3, 6} in 
+the array sums up to 10.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function find3Numbers() which takes the array arr[], the size of the array (n) and the sum (X) as inputs and returns True if there exists a triplet in the array arr[] which sums up to X and False otherwise.
+
+
+Expected Time Complexity: O(n2)
+Expected Auxiliary Space: O(1)
+
+
+SOLUTION :
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    bool find3Numbers(int A[], int n, int X)
+    {
+        //Your Code Here
+            int left, right;
+    sort(A,A+n);
+    for(int i=0;i<n;i++){
+        left= i+1;
+        right= n-1;
+       while(left<right){
+        int currsum = A[i] + A[left] + A[right];
+        if(currsum == X){
+        return true;}
+        
+        else if(currsum <X){
+            left++;
+        }
+        else if(currsum >X){
+            right--;
+        }
+        
+    } 
+    }
+    return false;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		int n,X;
+		cin>>n>>X;
+		int i,A[n];
+		for(i=0;i<n;i++)
+			cin>>A[i];
+		Solution ob;
+        cout <<  ob.find3Numbers(A, n, X) << endl;
+    }
+}
+
+// } Driver Code Ends
