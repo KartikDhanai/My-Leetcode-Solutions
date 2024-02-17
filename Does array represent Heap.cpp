52e8143b38a1f9515103a805662f93729fdf5bfe
Kartik Dhanai <94917152+KartikDhanai@users.noreply.github.com>
2024-02-17 22:31:42 +0530

    Create Does array represent Heap.cpp

---
+++ Does array represent Heap.cpp
@@ -0,0 +1,91 @@
+/*Given an array arr of size n, the task is to check if the given array can be a level order representation of a Max Heap.
+
+Example 1:
+
+Input:
+n = 6
+arr[] = {90, 15, 10, 7, 12, 2}
+Output: 
+1
+Explanation: 
+The given array represents below tree
+       90
+     /    \
+   15      10
+  /  \     /
+7    12  2
+The tree follows max-heap property as every
+node is greater than all of its descendants.
+Example 2:
+Input:  
+n = 6
+arr[] = {9, 15, 10, 7, 12, 11}
+Output:
+0
+Explanation:
+The given array represents below tree
+       9
+     /    \
+   15      10
+  /  \     /
+7    12  11
+The tree doesn't follows max-heap property 9 is
+smaller than 15 and 10, and 10 is smaller than 11. 
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function isMaxHeap() which takes the array arr[] and its size n as inputs and returns True if the given array could represent a valid level order representation of a Max Heap, or else, it will return False.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+*/
+
+Solution :
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution{
+    public:
+    bool isMaxHeap(int arr[], int n)
+    {
+        queue<int>q;
+        q.push(arr[0]);
+        for(int i=1;i<n;i++){
+            int big= q.front();
+            q.pop();
+            if(arr[i] > big)
+            return 0;
+            q.push(arr[i]);
+            if(i+1<n){
+                if(arr[i+1]>big)
+                return 0;
+                
+                q.push(arr[i+1]);
+                i++;
+            }
+        }
+        return 1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+       int n;
+       cin >> n;
+       int a[4*n]={0};
+       for(int i =0;i<n;i++){
+           cin >> a[i];
+       }
+       Solution ob;
+       cout<<ob.isMaxHeap(a, n)<<endl;
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
